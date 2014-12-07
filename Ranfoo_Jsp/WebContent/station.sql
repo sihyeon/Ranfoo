6b38fbf0730e7dd217dff037f32563c6d998779b
YoungjinJoo <jkevin0119@naver.com>
2014-12-07 23:34:06 +0900

    fix sql
    sql문 수정

--- Ranfoo_Jsp/WebContent/station.sql
+++
@@ -1,8 +0,0 @@
-create database station;
-grant all on station.* to 'id02'@'localhost' identified by 'pwd02';
-grant all on station.* to 'id02'@'%' identified by 'pwd02';
-
-create TABLE stations (
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	station VARCHAR(80) NOT NULL,
-	memberNum VARCHAR(80) NOT NULL);
